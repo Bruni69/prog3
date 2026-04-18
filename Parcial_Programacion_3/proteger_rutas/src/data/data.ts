b1c522d69bdde10acdf743260d216c745cbdfac0
Bruno <bldesinfecciones@gmail.com>
2026-04-18 11:37:36 -0300

    trabajando en el primer parcial Prog3

---
+++ Parcial_Programacion_3/proteger_rutas/src/data/data.ts
@@ -0,0 +1,39 @@
+import type {IProduct} from "../types/product";
+export const productos: IProduct[]=[
+    {
+        id:1,
+        nombre:"Hamburguesa Triple",
+        descripcion:"Triple de carne, cheddar y bacon",
+        precio: 25000,
+        imagen: "./assets/hamburguesa.png",
+        categoria: "Hamburguesa"
+    },
+    { 
+         id:2,
+        nombre:"Pizza muzzarela Clasica",
+        descripcion:"Pizza de masas al molde con muzzarela y aceitunas",
+        precio: 28000,
+        imagen: "./assets/pizza.png",
+        categoria: "Pizzas"
+
+    },
+    { 
+         id:3,
+        nombre:"Papas Fritas Especial",
+        descripcion:"bastones de papas fritas con cheddar y bacon",
+        precio: 15000,
+        imagen: "./assets/papas_fritas.png",
+        categoria: "Papas Fritas"
+
+    },
+    { 
+         id:4,
+        nombre:"Bebida lima limon",
+        descripcion:"Gaseosa linea Pepsi",
+        precio: 5000,
+        imagen: "./assets/lima.png",
+        categoria: "Bebida"
+
+    }
+   
+]  
---
+++ Parcial_Programacion_3/proteger_rutas/src/pages/store/home/home.ts
@@ -0,0 +1,39 @@
+import {productos} from "../../data/data";
+const cargarCategorias = () => {
+    const listaCategorias = document.getElementById('lista-categorias');
+
+    // CORRECCIÓN: Quitamos la tilde a 'categorias' 
+    categorias.forEach(categoria => {
+        const li = document.createElement('li');
+        li.innerHTML = `<a href="#">${categoria}</a>`;
+        listaCategorias.appendChild(li); // [cite: 119]
+    });
+};
+
+const cargarProductos = () => {
+    // CORRECCIÓN: Usamos el ID exacto de tu HTML [cite: 37]
+    const contenedor = document.getElementById('productos-destacados');
+
+    productos.forEach(producto => {
+        const article = document.createElement('article');
+        // Usamos Template Strings para inyectar los datos [cite: 124, 125]
+        article.innerHTML = `
+            <img src="${producto.imagen}" width="150" alt="${producto.nombre}">
+            <h3><strong>${producto.nombre}</strong></h3>
+            <p>${producto.descripcion}</p>
+            <p>Precio: <strong>$${producto.precio.toFixed(2)}</strong></p>
+            <button type="button">Ver Detalles</button>
+            <button type="button" onclick="agregarAlCarrito('${producto.nombre}')">Agregar al Carrito</button>
+        `;
+        contenedor.appendChild(article); // [cite: 123]
+    });
+};
+
+// FUNCIÓN DE APOYO (Paso 3.B)
+const agregarAlCarrito = (nombre) => {
+    alert("Has seleccionado: " + nombre); // 
+};
+
+// FUNDAMENTAL: Ejecutar las funciones para que se vea en la página 
+cargarCategorias();
+cargarProductos();
